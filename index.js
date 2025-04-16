19fc174bbb7698f2db5a08b810730d8e2097d7bb
p <jlewis0613@outlook.com>
2025-04-16 19:45:15 -0400

    poem function

--- index.js
+++ index.js
@@ -9,7 +9,30 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // complete this function
-const makePoemHTML = () => {}
+const makePoemHTML = ([poem]) => {
+  const { title, author, lines } = poem;
+
+  const makeH2 = makeTag('h2');
+  const makeH3 = makeTag('h3');
+  const makeEm = makeTag('em');
+  const makeP = makeTag('p');
+
+  const makeTitle = () => makeH2(title);
+  const makeAuthor = () => makeH3(makeEm(`by ${author}`));
+
+  const makeStanza = pipe(
+    stanzaLines => stanzaLines.join('<br>'),
+    makeP
+  );
+
+  const stanzas = lines.reduce((acc, line) => {
+    if (line === '') acc.push([]);
+    else acc[acc.length - 1].push(line);
+    return acc;
+  }, [[]]).map(makeStanza).join('');
+
+  return `${makeTitle()}${makeAuthor()}${stanzas}`;
+};
 
 // attach a click event to #get-poem
 getPoemBtn.onclick = async function() {
